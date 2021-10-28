058d0edb9c303b3672759ad1021b079ca8101177
Andy Chou <andychchou@gmail.com>
2021-10-28 10:57:15 -0700

    Scaffold

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$('#about-tab').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+$('#projects-tab').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
+$('#contact-tab').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+})
