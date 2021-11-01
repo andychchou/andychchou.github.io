0e3ef40df33b5546dceb6506690e6918ccb5505c
Andy Chou <andychchou@gmail.com>
2021-10-31 21:28:40 -0700

    Page Content Added

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,64 @@
-$('#about-tab').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
+// //Bootstrap 4 jquery
+// $('#about-tab').on('click', function (e) {
+//     e.preventDefault()
+//     $(this).tab('show')
+// })
+// $('#projects-tab').on('click', function (e) {
+//     e.preventDefault()
+//     $(this).tab('show')
+// })
+// $('#resume-tab').on('click', function (e) {
+//     e.preventDefault()
+//     $(this).tab('show')
+// })
+// $('#contact-tab').on('click', function (e) {
+//     e.preventDefault()
+//     $(this).tab('show')
+// })
+
+//Bootstrap 5 js
+var triggerTabList = [].slice.call(document.querySelectorAll('#about-tab'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
 })
-$('#projects-tab').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
+var triggerTabList = [].slice.call(document.querySelectorAll('#projects-tab'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
 })
-$('#resume-tab').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
+var triggerTabList = [].slice.call(document.querySelectorAll('#experience-tab'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
 })
-$('#contact-tab').on('click', function (e) {
-    e.preventDefault()
-    $(this).tab('show')
+var triggerTabList = [].slice.call(document.querySelectorAll('#resume-tab'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
+var triggerTabList = [].slice.call(document.querySelectorAll('#contact-tab'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
 })
